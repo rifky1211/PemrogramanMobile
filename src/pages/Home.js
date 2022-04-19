c54647ab76313c1700ac16d410e02624e8829d98
rifky1211 <rifky363@gmail.com>
2022-04-19 14:35:51 +0700

    tugas 4 done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   ScrollView,
   View,
@@ -6,12 +6,17 @@ import {
   Image,
   Alert,
   BackHandler,
+  TextInput,
   Text,
 } from 'react-native';
+
 
 import ImageSlider from '../components/ImageSlider';
 
 export default function Home() {
+  const [massa, setMassa] = useState(0);
+  const [result, setResult] = useState(null)
+
   useEffect(() => {
     const backAction = () => {
       Alert.alert('Exit', 'Are you sure you want to exit?', [
@@ -50,12 +55,21 @@ export default function Home() {
           </Text>
         </View>
         <ImageSlider />
-        <View>
-          <Text style={{color: 'black'}}>Nama Saya Rifky Ardiansyah</Text>
-          <Text style={{color: 'black'}}>Nama Saya 20180801357</Text>
-          <Text style={{color: 'black'}}>Hobby Saya bermain badminton</Text>
-          <Text style={{color: 'black'}}>Pekerjaan saya sebagai Backend Developer</Text>
-          <Text style={{color: 'black'}}>Status saya belum menikah</Text>
+        <View style={{marginTop: 10}}>
+          <TextInput
+            style={{height: 40, color:"white", backgroundColor: "black"}}
+            placeholder="Type here to convert kilogram to another unit"
+            onChangeText={e => setMassa(e)}
+            value={massa}
+            keyboardType="numeric"
+          />
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa * 35.274} Ons</Text>
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa * 2.205} Pons</Text>
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa * 6.35} Stone</Text>
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa * 907} Ton Pendek</Text>
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa * 1016} Ton Panjang</Text>
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa * 35274} Miligram</Text>
+          <Text style={{color:"black", marginBottom: 18}}>{massa} Kilogram = {massa / 1000} Ton Metrik</Text>
         </View>
       </ScrollView>
     </View>
